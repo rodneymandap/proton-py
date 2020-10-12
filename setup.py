3d484fa93eba1bbc8c309569fd9e35f7bc02084f
rodney <rodneyjan.mandap@iwgplc.com>
2020-10-12 16:04:02 +0800

    Add new classifiers

--- setup.py
+++ setup.py
@@ -4,16 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="iwgproton",
+    name="iwg-proton",
     version="0.0.1",
     author="rodneymandap",
-    author_email="rodneyjan.mandap@gmail.com",
+    author_email="rodneyjan.mandap@iwgplc.com",
     description="Python package to connect to IWG Proton API",
     long_description=long_description,
     url="https://github.com/rodneymandap/iwg-proton",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3',
+    install_requires=['requests'],
 )
