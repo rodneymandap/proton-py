9892a40efeeba9c4102a59c4013e893e3dd0b60f
rodney <rodneyjan.mandap@iwgplc.com>
2020-10-12 18:17:32 +0800

    Change name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="iwgproton",
+    name="proton",
     version="0.0.1",
     author="rodneymandap",
     author_email="rodneyjan.mandap@iwgplc.com",
