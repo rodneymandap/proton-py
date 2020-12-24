f8cfb80c9edbac60baae6585108fad89bf013b57
rodney <rodneyjan.mandap@iwgplc.com>
2020-12-24 12:21:07 +0800

    Change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="proton-py",
-    version="0.0.1",
+    version="0.1.0",
     author="rodneymandap",
     author_email="rodneyjan.mandap@gmail.com",
     description="Python package to connect to IWG Proton API",
